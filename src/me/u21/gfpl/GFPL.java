28459ec490528922c7e56fb24482acf1e9adc866
u21 <u21.business@protonmail.com>
2018-04-18 09:25:53 +0100

    Update GFPL.java

--- src/me/u21/gfpl/GFPL.java
+++ src/me/u21/gfpl/GFPL.java
@@ -1,1 +1,13 @@
-sd
+package me.u21.gfpl;
+
+public class GFPL {
+ 
+  public void logicalapproach(){
+    Logic logic = new Logic();
+    logic == null;
+    if(logic != null){
+      System.out.printLn("You should probably kys in all fairness..");
+    }
+  }
+  
+}
